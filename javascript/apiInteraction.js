db78347646035fd06f59d8a1850ea64ff4d389cb
Quaker10 <steveszumski@Steves-MacBook-Pro.local>
2018-07-11 21:36:43 -0700

    added spotify web api and started spotify api queries

--- javascript/apiInteraction.js
+++ javascript/apiInteraction.js
@@ -1,5 +1,37 @@
-// function for querying Spotify API
+
+var spotifyApi = new SpotifyWebApi();
+
+//var artist = userInputArtist
+var artist="Bone thugs and harmony";
+//spotifyApi.setAccessToken('')
+
+// Querys the Spotify API for the Spotify Artist ID - 1 ajax call
+spotifyApi.searchArtists( artist, {}, function (err, data) {
+    if (err) console.error(err);
+    else console.log('Artist ID', data);
+    console.log()
+  });
 
 
-//function for querying SongKick API
+
+
+
+// use the artist ID to query Spotify and return related artists
+
+function returnRelatedArtists () {
+
+    var queryURL = "https://api.spotify.com/v1/artists/{id}/related-artists" + artist + "&type=track&limit=1";
+}
+
+
+//the user will click on a related artist, and the artist they click on will query the api to return that artist's music
+
+function returnRelatedArtistMusic () {
+
+
+
+}
+
+
+
 
