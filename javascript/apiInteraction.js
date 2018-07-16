cf285699812d9be85ddf17db0c75180cb853a1e9
Quaker10 <steveszumski@Steves-MacBook-Pro.local>
2018-07-16 16:04:32 -0700

    structured spotify setup

--- javascript/apiInteraction.js
+++ javascript/apiInteraction.js
@@ -1,9 +1,28 @@
 
-var spotifyApi = new SpotifyWebApi();
+//sets up Spotify
 
-//var artist = userInputArtist
-var artist="Bone thugs and harmony";
-//spotifyApi.setAccessToken('')
+function setupSpotify () {
+    var spotifyApi = new SpotifyWebApi();
+
+    // Grabbing access token using Spotify Web API
+    $.ajax({
+        url: 'https://accounts.spotify.com/api/token',
+        headers: {
+         'Authorization': 'Basic ' + btoa( 11061422aa1046b9ab9b1c6eb0193619 + ":" + cfa323c2f0414388a8538fd11aa57206) //client ID and client secret from Spotify for Dev Dashboard
+       }
+       }).success(function(response) {
+       // Your token will be in response.access_token
+       
+           spotifyApi.setAccessToken(response.access_token);
+          
+       });
+}
+
+
+
+//var for user input artist
+var originalArtist="";
+
 
 // Querys the Spotify API for the Spotify Artist ID - 1 ajax call
 spotifyApi.searchArtists( artist, {}, function (err, data) {
@@ -12,25 +31,22 @@ spotifyApi.searchArtists( artist, {}, function (err, data) {
     console.log()
   });
 
+  var originalArtistID = ''
 
 
+// Query the Spotify API with originalArtistID and return related artists
+
+//store the related artists in an array
+
+//populate the array into relatedArtistsDiv
+
+//event handler for onclick in relatedArtistsDiv
+
+//Query the spotify API to return relatedArtist top tracks
+
+//populate the relatedArtist top tracks into Playlist Div
 
 
-// use the artist ID to query Spotify and return related artists
-
-function returnRelatedArtists () {
-
-    var queryURL = "https://api.spotify.com/v1/artists/{id}/related-artists" + artist + "&type=track&limit=1";
-}
-
-
-//the user will click on a related artist, and the artist they click on will query the api to return that artist's music
-
-function returnRelatedArtistMusic () {
-
-
-
-}
 
 
 
