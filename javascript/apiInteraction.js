efaf48d643bf780dac40259fadc058b60ba2ab50
Quaker10 <steveszumski@Steves-MacBook-Pro.local>
2018-07-16 21:28:30 -0700

    working on spotify mock data

--- javascript/apiInteraction.js
+++ javascript/apiInteraction.js
@@ -31,14 +31,32 @@ spotifyApi.searchArtists( artist, {}, function (err, data) {
     console.log()
   });
 
-  var originalArtistID = ''
+  var originalArtistID = '';
 
 
 // Query the Spotify API with originalArtistID and return related artists
+function returnRelatedArtists () {
 
-//store the related artists in an array
+    //store the related artists in an array
+    relatedArtists { //make into 10 objects
+        name : ['Big Sean', 'Jeremih', 'J. Cole', '2 Chainz', 'Tyga', 'Bryson Tiller', 'Tory Lanez', 'Future', 'Wale', 'Meek Millz' ]; 
+        URL :  [ 'https://open.spotify.com/artist/0c173mlxpT3dSFRgMO8XPh?si=MB9-ujSXRCmOfRJHOxfzKA', 'https://open.spotify.com/artist/3KV3p5EY4AvKxOlhGHORLg?si=-g_6vSYyQl6rQ1eH7UkFLQ', 'https://open.spotify.com/artist/6l3HvQ5sa6mXTsMTB19rO5?si=DILl--Q8TjqAvJfx1s_BMQ', 'https://open.spotify.com/artist/17lzZA2AlOHwCwFALHttmp?si=Al9bICUAR4i6n8nYLtcufg', 'https://open.spotify.com/artist/5LHRHt1k9lMyONurDHEdrp?si=4McXNt2ySWq0wLRg86clkA', 'https://open.spotify.com/artist/2EMAnMvWE2eb56ToJVfCWs?si=dzdch5d8RjOrzMHDZpiZzw', 'https://open.spotify.com/artist/2jku7tDXc6XoB6MO2hFuqg?si=ic1WO0xyQaiIqYd60loooA', 'https://open.spotify.com/artist/1RyvyyTE3xzB2ZywiAwp0i?si=G8iTkn5SRIGYVYrTUdKcjQ', 'https://open.spotify.com/artist/67nwj3Y5sZQLl72VNUHEYE?si=I3sRihgSTQyLtTAET1JV7Q', 'https://open.spotify.com/artist/20sxb77xiYeusSH8cVdatc?si=WCmMmsT4QXe8ncDx1hvNOg'];
+    };
+    //populate the array into relatedArtistsDiv 
 
-//populate the array into relatedArtistsDiv
+        $('#artistname').on('click',
+
+    )
+            for (i = 0; i < relatedArtists.name.length; i++) {
+                $('#relatedArtist').append(relatedArtists.name[i]);
+
+            }
+        
+    
+}
+
+
+
 
 //event handler for onclick in relatedArtistsDiv
 
