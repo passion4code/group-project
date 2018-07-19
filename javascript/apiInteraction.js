1153d4264772284e783fe053acc07b811ef92279
Quaker10 <steveszumski@Steves-MacBook-Pro.local>
2018-07-18 17:55:48 -0700

    added iframes and made a 3rd div for songkick

--- javascript/apiInteraction.js
+++ javascript/apiInteraction.js
@@ -38,10 +38,8 @@ spotifyApi.searchArtists( artist, {}, function (err, data) {
 function returnRelatedArtists () {
 
     //store the related artists in an array
-    relatedArtists { //make into 10 objects
-        name : ['Big Sean', 'Jeremih', 'J. Cole', '2 Chainz', 'Tyga', 'Bryson Tiller', 'Tory Lanez', 'Future', 'Wale', 'Meek Millz' ]; 
-    //     URL :  [ 'https://open.spotify.com/artist/0c173mlxpT3dSFRgMO8XPh?si=MB9-ujSXRCmOfRJHOxfzKA', 'https://open.spotify.com/artist/3KV3p5EY4AvKxOlhGHORLg?si=-g_6vSYyQl6rQ1eH7UkFLQ', 'https://open.spotify.com/artist/6l3HvQ5sa6mXTsMTB19rO5?si=DILl--Q8TjqAvJfx1s_BMQ', 'https://open.spotify.com/artist/17lzZA2AlOHwCwFALHttmp?si=Al9bICUAR4i6n8nYLtcufg', 'https://open.spotify.com/artist/5LHRHt1k9lMyONurDHEdrp?si=4McXNt2ySWq0wLRg86clkA', 'https://open.spotify.com/artist/2EMAnMvWE2eb56ToJVfCWs?si=dzdch5d8RjOrzMHDZpiZzw', 'https://open.spotify.com/artist/2jku7tDXc6XoB6MO2hFuqg?si=ic1WO0xyQaiIqYd60loooA', 'https://open.spotify.com/artist/1RyvyyTE3xzB2ZywiAwp0i?si=G8iTkn5SRIGYVYrTUdKcjQ', 'https://open.spotify.com/artist/67nwj3Y5sZQLl72VNUHEYE?si=I3sRihgSTQyLtTAET1JV7Q', 'https://open.spotify.com/artist/20sxb77xiYeusSH8cVdatc?si=WCmMmsT4QXe8ncDx1hvNOg'];
-    };
+    relatedArtists ['Big Sean', 'Jeremih', 'J. Cole', '2 Chainz', 'Tyga', 'Bryson Tiller', 'Tory Lanez', 'Future', 'Wale', 'Meek Millz' ]; 
+    
     //populate the array into relatedArtistsDiv 
 
         $('#artistname').on('click',
